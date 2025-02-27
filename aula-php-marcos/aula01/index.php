8b321f1bdd6bbaad2471c989d3d5fd9a8ab28d16
Marcos Maues <yboostx@gmail.com>
2025-02-27 10:16:54 -0300

    27.02.25

---
+++ aula-php-marcos/aula01/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira aula php</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+                <li>inicio</li>
+                <li>contato</li>
+                <li>sobre</li>
+        </nav>
+    </header>
+    <?php
+    //comando básico de saída: echo
+        echo "Hello, world!";
+        ?>
+    <main></main>
+</body>
+</html>
