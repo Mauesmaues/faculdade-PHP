a7fb6ed49b30075ed0e6a4f7b251a7fdfb62eb6c
Marcos Maues <yboostx@gmail.com>
2025-04-01 10:23:00 -0300

    aula01-04

---
+++ atividade.avaliativa/cadastro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <link rel="styleSheet" href="cadastro.css">
+</head>
+<body>
+    <?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $dados = $_POST;
+
+        foreach($dados as $dadoTemp){
+            echo $dadoTemp . "<br>";
+        }
+
+        $consumoDiario = ($dados['consumo'] / 1000) * $dados['numHoras'];
+        echo "Consumo diário do aparelho: " . number_format($consumoDiario, 2, '.', '') . "<br>";
+        $consumoMensal = $consumoDiario * $dados['numDias'];
+        echo "Consumo mensal do aparelho: " . $consumoMensal . "<br>";
+        $consumoReais = $consumoMensal * $dados['valorHora'];
+        echo "Consumo do aparelho em R$: R$" . number_format($consumoReais, 2, '.', '') . "<br>";
+    }else{
+        echo "nenhum dado recebido";
+    }
+
+    if($consumoReais <= 5){
+        echo "Consumo baixo";
+    }elseif($consumoReais <= 10){
+        echo "Consumo moderado";
+    }else{
+        echo "Consumo elevado";
+    }
+    ?>
+    
+</body>
+</html>
