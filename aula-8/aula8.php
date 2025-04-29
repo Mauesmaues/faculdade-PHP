4b3e5bf18f9d04b55a45f05d96fac5c7bf90ff93
Marcos Maues <yboostx@gmail.com>
2025-04-29 11:48:36 -0300

    att

--- aula-8/aula8.php
+++ aula-8/aula8.php
@@ -20,6 +20,8 @@
     $resp = $banco->query($q);
     var_dump($resp);
 
+    
+
     $usu = $resp->fetch_object();
     print_r($usu);
 
@@ -27,18 +29,6 @@
     echo "<br> Usuario: " . $usu->usuario;
     echo "<br> Nome: " . $usu->nome;
     echo "<br> Senha: " . $usu->senha;
-
-    $dados = $_POST;
-
-    if($dados["usuario"] == $usu->usuario && $dados["senha"] == $usu->senha){
-        echo "<br> Usuario logado";
-    }else{
-        echo "<br> senha ou usuario incorreto";
-    }
     
-
-    
-
-
 
 ?>
--- aula-8/banco.php
+++ aula-8/banco.php
@@ -2,6 +2,7 @@
     $banco = new mysqli("localhost:3307", "root", "", "php-segunda-noite");
     
     function fazerLogin($usuario, $senha){
+        global $banco;
         $banco = new mysqli("localhost:3307", "root", "", "php-segunda-noite");
         $q = "SELECT * FROM usuarios WHERE usuario='usuario'";
         $resp = $banco->query($q);
